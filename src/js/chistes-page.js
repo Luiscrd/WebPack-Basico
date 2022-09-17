e688c4853ff1b8f51f658aaaef4066b6c8b95c52
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-09-17 10:09:30 +0200

    más cambios

--- src/js/chistes-page.js
+++ src/js/chistes-page.js
@@ -1,15 +1,18 @@
 import { obtenerChite } from "./http-provider";
+import { crearIndexHtml } from "./index-page";
+import { initUsuarios } from "./usuarios-page";
 
 const body = document.body;
-let btnChiste, olList; 
+let btnChiste, btnVolver, olList; 
 
 const crearChistesHtml = () => {
 
     const html = `
     <div class="d-flex align-items-center justify-content-between">
+        <button class="btn btn-danger" id="volver"><= Volver</button>
         <img src="./assets/img/logo.png" alt="Logo" class="logo">
         <h1 class="tit">Chistes Chuk Norris</h1>
-        <button class="btn btn-primary">Otro chiste</button>
+        <button class="btn btn-primary" id="chiste">Otro chiste</button>
     </div>
     
     <hr>
@@ -25,13 +28,34 @@ const crearChistesHtml = () => {
 const eventos = () =>{
 
     olList = document.querySelector('ol');
-    btnChiste = document.querySelector('button');
+    btnChiste = document.getElementById('chiste');
+    btnVolver = document.getElementById('volver');
 
     btnChiste.addEventListener('click', async () => {
 
         btnChiste.disabled = true;
         dibujarChiste( await obtenerChite() );
         btnChiste.disabled = false;
+    })
+
+    btnVolver.addEventListener('click', async () => {
+
+        document.body.innerHTML = '';
+        crearIndexHtml();
+
+        let btnChistes = document.getElementById('chistes');
+        let btnUsuarios = document.getElementById('usuarios');
+
+        btnChistes.addEventListener('click', async () => {
+            document.body.innerHTML = '';
+            initChistes();
+        })
+
+        btnUsuarios.addEventListener('click', async () => {
+            document.body.innerHTML = '';
+            initUsuarios();
+        })
+
     })
 
 }
--- src/js/usuarios-page.js
+++ src/js/usuarios-page.js
@@ -1,13 +1,17 @@
+import { initChistes } from "./chistes-page";
 import { obtenerUsuarios } from "./http-provider";
+import { crearIndexHtml } from "./index-page";
 
 let tbody;
 let correlativo = 0;
-const body  = document.body;
+let btnVolver;
+const body = document.body;
 
 const crearHtml = () => {
-    
+
     const html = `
     <div class="d-flex align-items-center justify-content-between">
+        <button class="btn btn-danger" id="volver"><= Volver</button>
         <h1 class="tit2">Usuarios</h1>
         <img src="./assets/img/usuarios.png" alt="Logo" class="logo2">
     </div>
@@ -28,39 +32,64 @@ const crearHtml = () => {
 
     const div = document.createElement('div');
     div.innerHTML = html;
-    body.appendChild( div );
+    body.appendChild(div);
 
     tbody = document.querySelector('tbody');
 
 }
 
-const crearFilaUsuario = ( usuario ) => {
-    
+const crearFilaUsuario = (usuario) => {
+
     correlativo++;
 
     const html = `
-        <td scope="col">${ usuario.id }</td>
-        <td scope="col">${ usuario.email}</td>
-        <td scope="col">${ usuario.first_name } ${ usuario.last_name }</td>
+        <td scope="col">${usuario.id}</td>
+        <td scope="col">${usuario.email}</td>
+        <td scope="col">${usuario.first_name} ${usuario.last_name}</td>
         <td scope="col">
-            <img class="img-thumbnail" src="${ usuario.avatar }">
+            <img class="img-thumbnail" src="${usuario.avatar}">
         </td>
     `;
 
     const tr = document.createElement('tr');
     tr.innerHTML = html;
 
-    tbody.appendChild( tr );
+    tbody.appendChild(tr);
 
 }
 
 
-export const initUsuarios = async() => {
+
+
+export const initUsuarios = async () => {
 
     crearHtml();
 
+    btnVolver = document.getElementById('volver');
+
+    btnVolver.addEventListener('click', async () => {
+
+        document.body.innerHTML = '';
+
+        crearIndexHtml();
+
+        let btnChistes = document.getElementById('chistes');
+        let btnUsuarios = document.getElementById('usuarios');
+
+        btnChistes.addEventListener('click', async () => {
+            document.body.innerHTML = '';
+            initChistes();
+        })
+
+        btnUsuarios.addEventListener('click', async () => {
+            document.body.innerHTML = '';
+            initUsuarios();
+        })
+
+    })
+
     correlativo = 0;
 
-    (await obtenerUsuarios()).forEach( crearFilaUsuario );
+    (await obtenerUsuarios()).forEach(crearFilaUsuario);
 }
 
